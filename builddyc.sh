043fdc464e66e35e4cc801765c2cecc81304e0de
Yuchen <yuchendai@yahoo-inc.com>
2016-03-12 21:14:59 -0800

    fix builddyc.sh on Mac
    fix openssl error at make phase
    fix luajit build error due to force gcc opt

--- builddyc.sh
+++ builddyc.sh
@@ -1,1 +1,11 @@
-autoreconf -if && ./configure --prefix=/opt/ats-dyc --enable-debug --enable-cppapi && make
+LUAJITOPT=""
+if [ $(uname) == "Darwin" ]; then
+#packages
+brew install automake autoconf libtool pcre openssl
+brew link --force openssl
+LUAJITOPT="--disable-luajit"
+fi
+#config && make
+autoreconf -if && ./configure --prefix=/opt/ats-dyc --enable-debug --enable-cppapi ${LUAJITOPT} && make
+
+
